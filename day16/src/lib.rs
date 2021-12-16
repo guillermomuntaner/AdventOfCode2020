9f8e7bead01e1dd59ba181309f3d2b8ffedf80ac
Guillermo Muntaner <guillermomp87@gmail.com>
2021-12-17 00:20:02 +0100

    Day 16 - Optimizations removing all char and string conversions and doing decimal to binary conversions manually

--- day16/src/lib.rs
+++ day16/src/lib.rs
@@ -1,104 +1,78 @@
-pub fn part1(input: &str) -> usize {
-    let mut iterator = parse_input(input);
+pub fn part1(input: &str) -> u32 {
+    let mut iterator = input.chars().flat_map(to_binary);
     let (version_sum, _, _) = parse_packet(&mut iterator);
     version_sum
 }
 
-pub fn part2(input: &str) -> usize {
-    let mut iterator = parse_input(input);
+pub fn part2(input: &str) -> u64 {
+    let mut iterator = input.chars().flat_map(to_binary);
     let (_, _, value) = parse_packet(&mut iterator);
     value
 }
 
-fn parse_input(input: &str) -> impl Iterator<Item = char> + '_ {
-    input.chars().flat_map(|hex| match hex {
-        '0' => vec!['0', '0', '0', '0'],
-        '1' => vec!['0', '0', '0', '1'],
-        '2' => vec!['0', '0', '1', '0'],
-        '3' => vec!['0', '0', '1', '1'],
-        '4' => vec!['0', '1', '0', '0'],
-        '5' => vec!['0', '1', '0', '1'],
-        '6' => vec!['0', '1', '1', '0'],
-        '7' => vec!['0', '1', '1', '1'],
-        '8' => vec!['1', '0', '0', '0'],
-        '9' => vec!['1', '0', '0', '1'],
-        'A' => vec!['1', '0', '1', '0'],
-        'B' => vec!['1', '0', '1', '1'],
-        'C' => vec!['1', '1', '0', '0'],
-        'D' => vec!['1', '1', '0', '1'],
-        'E' => vec!['1', '1', '1', '0'],
-        'F' => vec!['1', '1', '1', '1'],
-        _ => panic!("Unexpected char {}", hex),
-    })
+fn to_binary(hex: char) -> Vec<u32> {
+    let mut bits: Vec<u32> = Vec::new();
+    let mut decimal = hex.to_digit(16).unwrap();
+    for _ in 0..4 {
+        bits.insert(0,decimal % 2);
+        decimal /= 2
+    }
+    bits
 }
 
-fn parse_packet(iterator: &mut impl Iterator<Item = char>) -> (usize, usize, usize) {
-    let version = u8::from_str_radix(
-        &vec![
-            iterator.next().unwrap(),
-            iterator.next().unwrap(),
-            iterator.next().unwrap(),
-        ]
-        .iter()
-        .collect::<String>(),
-        2,
-    )
-    .unwrap();
-    let type_id = u8::from_str_radix(
-        &vec![
-            iterator.next().unwrap(),
-            iterator.next().unwrap(),
-            iterator.next().unwrap(),
-        ]
-        .iter()
-        .collect::<String>(),
-        2,
-    )
-    .unwrap();
+fn parse_packet(iterator: &mut impl Iterator<Item = u32>) -> (u32, u32, u64) {
+    let mut version = 0;
+    for _ in 0..3 {
+        version = (version << 1) + iterator.next().unwrap()
+    }
 
-    let mut version_sum = version as usize;
+    let mut type_id = 0;
+    for _ in 0..3 {
+        type_id = (type_id << 1) + iterator.next().unwrap()
+    }
+
+    let mut version_sum = version;
     let mut body_bit_count = 0;
-    let value;
 
-    if type_id == 4 {
+    let value = if type_id == 4 {
         // Literal value
-        let mut literal_value_binary: Vec<char> = Vec::new();
+        let mut literal_value = 0;
         loop {
             let group_prefix = iterator.next().unwrap();
-            literal_value_binary.push(iterator.next().unwrap());
-            literal_value_binary.push(iterator.next().unwrap());
-            literal_value_binary.push(iterator.next().unwrap());
-            literal_value_binary.push(iterator.next().unwrap());
+            for _ in 0..4 {
+                literal_value = (literal_value << 1) + iterator.next().unwrap()
+            }
             body_bit_count += 5;
-            if group_prefix == '0' {
+            if group_prefix == 0 {
                 break;
             }
         }
-        // Skip filling bits until multiple of 5
-        for _ in 0..(body_bit_count % 5) {
-            iterator.next();
-            body_bit_count += 1;
-        }
 
-        value = usize::from_str_radix(&literal_value_binary.iter().collect::<String>(), 2).unwrap();
+        // Skip filling bits until multiple of 5
+        let filling_zeros = body_bit_count % 5;
+        for _ in 0..filling_zeros {
+            iterator.next();
+        }
+        body_bit_count += filling_zeros;
+
+        literal_value as u64
     } else {
         // Operator
         let lenght_type_id = iterator.next().unwrap();
         body_bit_count += 1;
 
-        let values = match lenght_type_id {
-            '0' => {
+        let values: Vec<u64> = match lenght_type_id {
+            0 => {
                 // If the length type ID is 0, then the next 15 bits are a number that represents
                 // the total length in bits of the sub-packets contained by this packet.
-                let mut value: Vec<char> = Vec::new();
+                let mut length_in_bits = 0;
                 for _ in 0..15 {
-                    value.push(iterator.next().unwrap());
-                    body_bit_count += 1;
+                    length_in_bits = (length_in_bits << 1) + iterator.next().unwrap()
                 }
-                let length_in_bits = usize::from_str_radix(&value.iter().collect::<String>(), 2).unwrap();
+                body_bit_count += 15;
 
-                let mut values: Vec<usize> = Vec::new();
-                let mut sub_packages_bit_count = 0_usize;
+                let mut values: Vec<u64> = Vec::new();
+                let mut sub_packages_bit_count = 0;
                 while sub_packages_bit_count < length_in_bits {
                     let (version, bit_count, value) = parse_packet(iterator);
                     sub_packages_bit_count += bit_count;
@@ -114,17 +88,16 @@ fn parse_packet(iterator: &mut impl Iterator<Item = char>) -> (usize, usize, usi
                 }
                 values
             }
-            '1' => {
+            1 => {
                 // If the length type ID is 1, then the next 11 bits are a number that
                 // represents the number of sub-packets immediately contained by this packet.
-                let mut value: Vec<char> = Vec::new();
+                let mut number_of_sub_packets = 0;
                 for _ in 0..11 {
-                    value.push(iterator.next().unwrap());
-                    body_bit_count += 1;
+                    number_of_sub_packets = (number_of_sub_packets << 1) + iterator.next().unwrap()
                 }
-                let number_of_sub_packets = u8::from_str_radix(&value.iter().collect::<String>(), 2).unwrap();
+                body_bit_count += 11;
 
-                let mut values: Vec<usize> = Vec::new();
+                let mut values = Vec::new();
                 for _ in 0..number_of_sub_packets {
                     let (version, bit_count, value) = parse_packet(iterator);
                     body_bit_count += bit_count;
@@ -136,36 +109,17 @@ fn parse_packet(iterator: &mut impl Iterator<Item = char>) -> (usize, usize, usi
             _ => panic!("Unexpected lenght_type_id={}", lenght_type_id),
         };
 
-        value = match type_id {
-            // sum
+        match type_id {
             0 => values.iter().sum(),
             1 => values.iter().product(),
             2 => *values.iter().min().unwrap(),
             3 => *values.iter().max().unwrap(),
-            5 => {
-                if values[0] > values[1] {
-                    1
-                } else {
-                    0
-                }
-            }
-            6 => {
-                if values[0] < values[1] {
-                    1
-                } else {
-                    0
-                }
-            }
-            7 => {
-                if values[0] == values[1] {
-                    1
-                } else {
-                    0
-                }
-            }
+            5 => (values[0] > values[1]) as u64,
+            6 => (values[0] < values[1]) as u64,
+            7 => (values[0] == values[1]) as u64,
             _ => panic!("Unexpected lenght_type_id={}", lenght_type_id),
         }
-    }
+    };
 
     (version_sum, 6 + body_bit_count, value)
 }
